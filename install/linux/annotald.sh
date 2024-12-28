f35c49d719bfe82b820c00090e870955771f2893
Luiz Veronesi <luiz.veronesi@outlook.com>
2024-12-28 11:51:43 +0100

    tested in ubuntu with zenity

--- install/linux/annotald.sh
+++ install/linux/annotald.sh
@@ -7,7 +7,7 @@ if ! command -v zenity &> /dev/null; then
 fi
 
 # Open file chooser dialog to select a PSD file
-filePath=$(zenity --file-selection --title="Select a PSD file to edit" --file-filter="*.psd")
+filePath=$(zenity --file-selection --title="Select a PSD file to edit")
 
 # Check if a file was selected
 if [[ -z "$filePath" ]]; then
@@ -19,12 +19,4 @@ fi
 folder=$(dirname "$filePath")
 filename=$(basename "$filePath")
 
-docker rm -f annotald || true
-docker run -it --rm --name annotald -p 8080:8080 -v "$folder":/data tychobrahe/annotald python bin/annotald /data/"$filename"
-
-# Open the default web browser to access the service
-if command -v xdg-open &> /dev/null; then
-    xdg-open http://localhost:8080
-else
-    echo "xdg-open is not available. Open your browser and go to http://localhost:8080"
-fi
+docker run --rm --name annotald -p 8080:8080 -v "$folder":/data tychobrahe/annotald python bin/annotald /data/"$filename"
